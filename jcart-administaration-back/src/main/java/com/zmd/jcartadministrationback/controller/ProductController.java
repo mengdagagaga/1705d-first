c3feea600f53c54b474a03a04433d1886fa6671a
Bill <www.15751458240@163.com>
2020-02-27 14:00:21 +0800

    add peoduct search page

--- jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/controller/ProductController.java
+++ jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/controller/ProductController.java
@@ -22,6 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/product")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
---
+++ jcart-administration-front/viewmodels/product-search.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo:'',
+        pageNum:1
+    },
+    mounted() {
+        console.log('view mounted');
+        this.searchProduct();
+    },
+    methods: {
+        handlePageChange(val){
+            console.log('page change');
+            this.pageNum=vall;
+            this.searchProduct();
+        },
+
+        searchProduct(){
+            axios.get('/product/search',{
+                params:{
+                    pageNum:this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
+
+
+})
