4286beced351fa98d756f73db52f0f1a0626f5cb
Bill <www.15751458240@163.com>
2020-03-04 13:28:16 +0800

    删除购物车里的商品

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -27,7 +27,14 @@ var app = new Vue({
         this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
     },
     methods: {
-        
+        handleDelete(index, row) {
+            console.log('delete click');
+            if (confirm('确定删除？')) {
+                this.myShoppingCart.splice(index, 1);
+                localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+                this.$message.success('删除购物车成功');
+            }
+        },
         handleUpdate(index, row) {
             console.log('update click');
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
