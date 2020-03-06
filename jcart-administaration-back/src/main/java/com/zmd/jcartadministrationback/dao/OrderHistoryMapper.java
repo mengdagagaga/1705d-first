b1013631eefdb32c47775aa836804322d56b1b8c
Bill <www.15751458240@163.com>
2020-03-06 15:40:32 +0800

    修改了一下 订单历史表的id类型

--- jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/dao/OrderHistoryMapper.java
+++ jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/dao/OrderHistoryMapper.java
@@ -21,6 +21,7 @@ public interface OrderHistoryMapper {
     int updateByPrimaryKey(OrderHistory record);
 
 
-    //my
-    List<OrderHistory> selectByOrderId(@Param("orderId")Long orderId);
+    //    custom
+
+    List<OrderHistory> selectByOrderId(@Param("orderId") Long orderId);
 }
