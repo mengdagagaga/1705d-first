dfc27ff29ca0cf9080e605fab23ae0ecb00c4592
Bill <www.15751458240@163.com>
2020-03-06 14:27:32 +0800

    根据客户id查询该客户所有地址

--- jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/controller/AddressController.java
+++ jcart-administaration-back/src/main/java/com/zmd/jcartadministrationback/controller/AddressController.java
@@ -1,12 +1,13 @@
 package com.zmd.jcartadministrationback.controller;
 
 import com.zmd.jcartadministrationback.dto.out.AddressListOutDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.zmd.jcartadministrationback.po.Address;
+import com.zmd.jcartadministrationback.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author ZMD
@@ -15,12 +16,29 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/address")
+@CrossOrigin
 public class AddressController {
 
 
+    @Autowired
+    private AddressService addressService;
+
     @GetMapping("/getListByCustomerId")
     public List<AddressListOutDTO> getListByCustomerId(@RequestParam Integer customerId){
-        return null;
+
+        List<Address> addresses = addressService.getByCustomerId(customerId);
+        List<AddressListOutDTO> addressListOutDTOS = addresses.stream().map(address -> {
+            AddressListOutDTO addressListOutDTO = new AddressListOutDTO();
+            addressListOutDTO.setAddressId(address.getAddressId());
+            addressListOutDTO.setContent(address.getContent());
+            addressListOutDTO.setReceiverMobile(address.getReceiverMobile());
+            addressListOutDTO.setReceiverName(address.getReceiverName());
+            addressListOutDTO.setTag(address.getTag());
+            return addressListOutDTO;
+
+        }).collect(Collectors.toList());
+
+        return addressListOutDTOS;
     }
 
 
