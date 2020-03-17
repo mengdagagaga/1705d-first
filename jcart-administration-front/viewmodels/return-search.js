531634b195d4dba3e535149e08e5897a346d8d5b
Bill <www.15751458240@163.com>
2020-03-17 08:31:36 +0800

    return search（administrator）

--- jcart-administration-front/viewmodels/return-search.js
+++ jcart-administration-front/viewmodels/return-search.js
@@ -2,13 +2,44 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        returnId: '',
+        orderId: '',
+        customerName: '',
+        productCode: '',
+        productName: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ],
+        startTime: '',
+        endTime: '',
+        pageNum: 1,
     },
     mounted() {
         console.log('view mounted');
         this.searchReturn();
     },
     methods: {
+        handleSearchClick() {
+            console.log('search click');
+            this.pageNum = 1;
+            this.searchReturn();
+        },
+        handleClearClick() {
+            console.log('clear click');
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName = '';
+            this.productCode = '';
+            this.productName = '';
+            this.selectedStatus = '';
+            this.startTime = '';
+            this.endTime = '';
+        },
         handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
